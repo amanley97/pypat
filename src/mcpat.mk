336edd171855f82dac43898593b9f489a187e41b
Alex Manley <amanley97@ku.edu>
2025-05-30 14:02:29 -0500

    setup mcpat subdir

--- src/mcpat.mk
+++ src/mcpat.mk
@@ -7,7 +7,6 @@ ifndef NTHREADS
   NTHREADS = 4
 endif
 
-
 LIBS = 
 INCS = -lm
 
@@ -17,13 +16,11 @@ ifeq ($(TAG),dbg)
 else
   DBG = 
   OPT = -O3 -msse2 -mfpmath=sse -DNTHREADS=$(NTHREADS) -Icacti
-  #OPT = -O0 -DNTHREADS=$(NTHREADS)
 endif
 
-#CXXFLAGS = -Wall -Wno-unknown-pragmas -Winline $(DBG) $(OPT) 
 CXXFLAGS = -Wno-unknown-pragmas $(DBG) $(OPT) 
-CXX = g++ -m32
-CC  = gcc -m32
+CXX = g++
+CC  = gcc
 
 VPATH = cacti
 
@@ -65,13 +62,10 @@ SRCS  = \
 OBJS = $(patsubst %.cc,obj_$(TAG)/%.o,$(SRCS))
 
 all: obj_$(TAG)/$(TARGET)
-	cp -f obj_$(TAG)/$(TARGET) $(TARGET)
+	cp -f obj_$(TAG)/$(TARGET) ../$(TARGET)
 
 obj_$(TAG)/$(TARGET) : $(OBJS)
 	$(CXX) $(OBJS) -o $@ $(INCS) $(CXXFLAGS) $(LIBS) -pthread
-
-#obj_$(TAG)/%.o : %.cc
-#	$(CXX) -c $(CXXFLAGS) $(INCS) -o $@ $<
 
 obj_$(TAG)/%.o : %.cc
 	$(CXX) $(CXXFLAGS) -c $< -o $@
